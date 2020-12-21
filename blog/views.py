7f41447a86f41fcf929dd68fb5649720092cc4ca
medteuw <medteuw@gmail.com>
2020-12-21 00:16:04 +0000

    using templates

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
+posts = [
+    {
+        'author' : 'Mohamed teuw',
+        'title' : 'first post',
+        'date': 'December 20, 2020 - 21:36:21',
+        'content' : 'this the content of the first post'
+    },
+    {
+        'author' : 'Yeslem',
+        'title' : 'Second post',
+        'date': 'December 20, 2020 - 21:36:21',
+        'content' : 'this the content of the second post'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts' : posts,}
+    
+    return render(request, 'blog/home.html',context)
 
 def about(request):
-    return HttpResponse("<h1>Blog about</h1>")
+    return render(request,'blog/about.html', {'title' : 'About'})
 
 
 def contact(request):
-    return HttpResponse('<h2>blog contact</h2>')
+    return render(request, 'blog/contact.html')
